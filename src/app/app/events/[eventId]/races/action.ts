28573e7c14a72c6a6ed960ca51e4340e78949587
RolandRFuchs <rolandrfuchs92@gmail.com>
2024-05-05 15:02:08 +0200

    Add logic to handle participants who are part of a deleted race.

--- src/app/app/events/[eventId]/races/action.ts
+++ src/app/app/events/[eventId]/races/action.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { _db } from "@/lib/db";
+import { omit } from "lodash";
 import { revalidatePath, unstable_noStore } from "next/cache";
 
 export async function deleteRace(raceId: string) {
@@ -9,6 +10,30 @@ export async function deleteRace(raceId: string) {
       id: raceId,
     },
   });
+
+  const raceParticipants = await _db.participant.findMany({
+    where: {
+      batches: {
+        some: {
+          race_id: raceId,
+        },
+      },
+    },
+  });
+
+  await _db.$transaction(
+    raceParticipants.map((i) =>
+      _db.participant.update({
+        data: {
+          ...omit(i, "id"),
+          batches: i.batches.filter((b) => b.race_id !== raceId),
+        },
+        where: {
+          id: i.id,
+        },
+      }),
+    ),
+  );
 
   revalidatePath("");
 
