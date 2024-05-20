bec5e70819a752a3d8fc66ae616f0ec60a931e31
RolandRFuchs <rolandrfuchs92@gmail.com>
2024-05-20 19:22:30 +0200

    Add a click link button to hide layout on results page.

--- src/app/app/events/[eventId]/results/page.tsx
+++ src/app/app/events/[eventId]/results/page.tsx
@@ -1,16 +1,25 @@
 import React, { Suspense } from "react";
 
 import { LaneRaceResults } from "./LaneRaceResults";
+import { LinkButton } from "@/components/FormElements/button";
 
 interface ResultsPageProps {
   params: {
     eventId: string;
-  };
+  },
+  searchParams: {
+    hideLayout?: boolean
+  }
 }
 
-export default async function ResultsPage({ params }: ResultsPageProps) {
+export default async function ResultsPage({ params, searchParams }: ResultsPageProps) {
   return (
     <div>
+      {
+        searchParams.hideLayout
+          ? <LinkButton href={`/app/events/${params.eventId}/results`} label="Show Layout" />
+          : <LinkButton href={`/app/events/${params.eventId}/results?hideLayout=true`} label="Hide Layout" />
+      }
       <Suspense fallback="Loading...">
         <LaneRaceResults eventId={params.eventId} />
       </Suspense>
