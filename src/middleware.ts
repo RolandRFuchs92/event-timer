a3630966877ec294355cd2ed75c97505f5b731c3
RolandRFuchs <rolandrfuchs92@gmail.com>
2024-06-13 09:41:59 +0200

    Start making progress with authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
-import { auth } from "./auth";
-
-export default auth;
+import { auth } from "../auth";
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
+
+export default auth;
